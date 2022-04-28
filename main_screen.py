21341c3143dd81d7e6e7401b5bb589c1cf5a5ba5
K1ngSlam <leon.schugk@studmail.htw-aalen.de>
2022-04-28 23:14:23 +0200

    YAML integration so directorys/nodes work (projects still in progress)

--- main_screen.py
+++ main_screen.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 from logging import Logger
 from kivy.app import App
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, ListProperty
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import Screen
 from kivy.uix.treeview import TreeView, TreeViewNode
@@ -17,60 +17,69 @@ class MainScreen(MDScreen):
     def __init__(self, **kw):
         super().__init__(**kw)
         self.tv_list = []
+        self.node_list = []
+        self.project_list = []
+        self.yaml_list = ListProperty()
         self.focused_md_file = StringProperty("")
 
-    def on_pre_enter(self, *args):
-        # TODO suche in den Yaml datein nach den root directorys/Namen um trees zu erstellen
-        # self.tv_list.append(TreeView(root_options=dict(text="Hausaufgaben")))
-        self.tv_list.append(TreeView(root_options=dict(text="Hausarbeiten")))
-        for x in range(len(self.tv_list)):
-            self.ids.tree_views.add_widget(self.tv_list[x])
-            self.populate_tree_view(self.tv_list[x])
+    def on_pre_enter(self, *args):  #Sollte nur beim ersten mal die TreeViews populaten oder vorher immer alles löschen?
+        app = App.get_running_app()
+        self.yaml_list = fnmatch.filter(os.listdir(str(App.get_running_app().directory_path)), '*.yaml')
+        for file_name in self.yaml_list:
+            content = app.read_yaml_file(file_name)
+            if "type" in content:
+                if content.get("type") == "directory":
+                    self.tv_list.append(TreeView(root_options=dict(text=content.get("title"))))
+                if content.get("type") == "project":
+                    self.project_list.append(file_name)
+                if content.get("type") == "node":
+                    self.node_list.append(TreeViewButton(timestamp= file_name.rstrip(".yaml"), text=content.get("title"), on_press=self.write_text_to_codeinput,size_hint=(1, 0.01)))
+
+        if len(self.tv_list) != 0:
+            for tv in self.tv_list:
+                self.ids.tree_views.add_widget(tv)
+                self.populate_tree_view(tv)
 
     def create_markdown(self):
-        print("Main Window: Button clicked")
         now = datetime.now()
         mdFile = MdUtils(file_name=str(now), title='Markdown File Example')
         mdFile.create_md_file()
         self.create_yaml(now)
         self.add_button_widget(now)
-        print(len(fnmatch.filter(os.listdir(str(App.get_running_app().directory_path)), '*.yaml')))
 
     def create_yaml(self, markdown_timestamp):
         yamlName = str(markdown_timestamp) + '.yaml'
         print("Yaml name:", yamlName)
-        open(yamlName, 'x')
+        open(os.path.join(App.get_running_app().directory_path, yamlName), 'x')
 
     def add_button_widget(self, markdown_timestamp):
-        tvb = TreeViewButton(size_hint=(1, 0.1), text=str(markdown_timestamp))
-        tvb.bind(on_press=lambda *args: self.write_text_to_codeinput(markdown_timestamp))
+        tvb = TreeViewButton(timestamp=str(markdown_timestamp), size_hint=(1, 0.1), text=str(markdown_timestamp))
+        tvb.bind(on_press=self.write_text_to_codeinput)
         self.ids.tree_views.add_widget(tvb)
 
     def update_tree_view(
-            self):  # TODO sollte async sein falls sich im repo etwas ändert das es sofort aktualisiert wird
+            self):
         pass
 
     def populate_tree_view(self, tree_view):
         app = App.get_running_app()
-        yaml_files = fnmatch.filter(os.listdir(str(app.directory_path)), '*.yaml')
-        for x in range(
-                len(yaml_files)):  # Alle nodes in dem Tree danach abfrage ob node Notiz oder nochmal directory ist
-            timestamp = yaml_files[x].rstrip(".yaml")
-            tree_view.add_node(TreeViewButton(text=timestamp, size_hint=(1, 0.01),
-                                              on_press=lambda *args: self.write_text_to_codeinput(timestamp))) #Wird hier auch wirklich bei den verschiedenen Buttons zur laufzeit das richtige übergeben?
+        for node in self.node_list:# Alle nodes in dem Tree danach abfrage ob node Notiz oder nochmal directory ist
+            content = app.read_yaml_file(node.timestamp + ".yaml")
+            if content.get("directory") == tree_view.root.text:
+                tree_view.add_node(node)
 
-    def write_text_to_codeinput(self, markdown_timestamp):
-        self.focused_md_file = markdown_timestamp  # Aus der Yaml datei lesen
-        text = App.get_running_app().read_md_file(str(markdown_timestamp))
+    def write_text_to_codeinput(self, instance):
+        self.focused_md_file = instance.timestamp
+        text = App.get_running_app().read_md_file(instance.timestamp)
         self.ids.box_for_codeinput.text = text
 
-    def on_focus(self, instance, value): #Sollte mit Tastenkombination funktionieren also z.B. Strg + s
+    def on_focus(self, instance, value):  # Keyboard_on_key_down textinput zum speichern
         if not value and self.focused_md_file != "":
             App.get_running_app().save_to_md_file(self.focused_md_file, instance.text)
 
 
 class TreeViewButton(MDFlatButton, TreeViewNode):
-    pass
+    timestamp = StringProperty("")
 
 # class TreeViewOneLineListItem(OneLineListItem, TreeViewNode):
 #    pass
--- noteApp.py
+++ noteApp.py
@@ -7,7 +7,7 @@ from kivy.logger import Logger
 from kivy.properties import StringProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.app import MDApp
-
+import yaml
 
 class NoteApp(MDApp):
 
@@ -19,19 +19,38 @@ class NoteApp(MDApp):
 
     def build(self):
         self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Red"
+        self.theme_cls.primary_palette = "Purple"
         self.sm.add_widget(Builder.load_file(os.path.join(self.directory, "main_screen.kv")))
         self.sm.add_widget(Builder.load_file(os.path.join(self.directory, "setting_screen.kv")))
         self.sm.current = "MainScreen"
         return self.sm
 
     def read_md_file(self,md_file_name):
-        with open(os.path.join(self.directory_path, md_file_name + ".md")) as file:
-            content = file.read()
-        return content
+        if str(md_file_name).endswith(".md"):
+            with open(os.path.join(self.directory_path, md_file_name)) as file:
+                content = file.read()
+            return content
+        else:
+            with open(os.path.join(self.directory_path, md_file_name + ".md")) as file:
+                content = file.read()
+            return content
 
     def save_to_md_file(self, md_file_name,text):
-        with open(os.path.join(self.directory_path, str(md_file_name) + ".md"), "w") as file:
-            file.write(text)
+        if str(md_file_name).endswith(".md"):
+            with open(os.path.join(self.directory_path, str(md_file_name)), "w") as file:
+                file.write(text)
+        else:
+            with open(os.path.join(self.directory_path, str(md_file_name) + ".md"), "w") as file:
+                file.write(text)
+
+    def read_yaml_file(self, file_name):
+        if str(file_name).endswith(".yaml"):
+            with open(os.path.join(self.directory_path, str(file_name)), "r") as file:
+                return yaml.load(file, Loader=yaml.FullLoader)
+        else:
+            with open(os.path.join(self.directory_path, str(file_name) + ".yaml"), "r") as file:
+                return yaml.load(file, Loader=yaml.Loader)
+
+
 
 NoteApp().run()
